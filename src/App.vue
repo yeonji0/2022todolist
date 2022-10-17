fc9675d61710b3de546e70c7399d1a5c6d0541c7
yeonji0 <kyj0279@gmail.com>
2022-10-17 12:15:40 +0900

    s

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  test
+  <todoinput></todoinput>
+  <todolist></todolist>
+  <todofooter></todofooter>
 </template>
 
 <script>
+import Todofooter from './components/todofooter.vue'
+import todoinput from './components/todoinput.vue'
+import Todolist from './components/todolist.vue'
 export default {
+  components: { todoinput, Todolist,Todofooter, },
 
 }
 </script>
