1aeb06ba2996f22658ed6a2e29664374d3fbb218
yeonji0 <kyj0279@gmail.com>
2022-10-17 12:13:14 +0900

    todo

---
+++ src/components/todoinput.vue
@@ -0,0 +1,13 @@
+<template>
+  input
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/todolist.vue
@@ -0,0 +1,13 @@
+<template>
+  list
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
